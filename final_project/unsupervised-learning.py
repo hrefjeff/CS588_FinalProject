9f694c2ac3dca43cdc9a7f5a0769054e380d516e
Jeffrey Allen <jeffwayne256@gmail.com>
2024-04-15 20:56:04 -0500

    Added k means clustering

--- final_project/unsupervised-learning.py
+++ final_project/unsupervised-learning.py
@@ -13,53 +13,50 @@ from scipy.spatial.distance import cdist
 
 from sklearn.metrics import davies_bouldin_score, silhouette_score
 
+subject = 1
 exercise = 1
 unit = 2
 
-# Load dataset for each subject
-for subject in range(1, constants.SUBJECTS + 1):
-    x = pd.read_csv(f's{subject}/e{exercise}/u{unit}/test.txt', delimiter=';')
+# # Load dataset for each subject
+# for subject in range(1, constants.SUBJECTS + 1):
+#     x = pd.read_csv(f's{subject}/e{exercise}/u{unit}/test.txt', delimiter'=';')',
+#     # Drop the time index as it's not a feature
+#     x = x.drop(columns=['time index'])
 
-    # Drop the time index as it's not a feature
-    x = x.drop(columns=['time index'])
+#     # Data visualization
+#     plt.scatter(x['acc_x'], x['acc_y'], s=50)
+#     plt.xlabel('Accelerometer X', fontsize=14)
+#     plt.ylabel('Accelerometer Y', fontsize=14)
+#     plt.savefig(f'plots/scatterplot-s{subject}-e{exercise}-u{unit}.png')
+#     plt.figure()
 
-    # Data visualization
-    plt.scatter(x['acc_x'], x['acc_y'], s=50)
-    plt.xlabel('Accelerometer X', fontsize=14)
-    plt.ylabel('Accelerometer Y', fontsize=14)
-    plt.savefig(f'plots/scatterplot-s{subject}-e{exercise}-u{unit}.png')
-    plt.figure()
+# # TODO: Run PCA to reduce dimensions down to 2 and justify it
+# # TODO: Run clustering algorithm k means on each subject's e1-u2
 
-# TODO: Run PCA to reduce dimensions down to 2 and justify it
-# TODO: Run clustering algorithm k means on each subject's e1-u2
-
-exit()
+# exit()
 
 
-# Load Iris Data
-iris = load_iris()
-
-x = pd.DataFrame(iris.data, columns=['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'])
-y = pd.DataFrame(iris.target, columns=['Target'])
+x = pd.read_csv(f's{subject}/e{exercise}/u{unit}/test-labeled.csv', delimiter=';') # Drop the time index as it's not a feature
+x = x.drop(columns=['time index'])
 
 # Data visualization
-colors = np.array(['red', 'green', 'blue'])
+colors = np.array(['gray', 'green', 'red', 'blue'])
 fig, axes = plt.subplots(1, 2, figsize=(12,4))
-axes[0].scatter(x['Sepal Length'], x['Sepal Width'], c=colors[y['Target']], s=50)
-axes[1].scatter(x['Petal Length'], x['Petal Width'], c=colors[iris.target], s=50)
-axes[0].set_xlabel('Sepal length', fontsize=14)
-axes[0].set_ylabel('Sepal width', fontsize=14)
-axes[1].set_xlabel('Petal length', fontsize=14)
-axes[1].set_ylabel('Petal width', fontsize=14)
+axes[0].scatter(x['acc_x'], x['acc_y'], c=colors[x['label']], s=50)
+axes[1].scatter(x['acc_x'], x['mag_x'], c=colors[x['label']], s=50)
+axes[0].set_xlabel('acc_x', fontsize=14)
+axes[0].set_ylabel('acc_y', fontsize=14)
+axes[1].set_xlabel('acc_x', fontsize=14)
+axes[1].set_ylabel('mag_x', fontsize=14)
 
-fig.savefig('iris-scatterplot.png')
+fig.savefig('plots/unsupervised/unsup-scatterplot.png')
 plt.figure()
 
 ###### Step 1: Pre-clustering
 
 # K-Means clustering
 cost = []
-for i in range(1,7):
+for i in range(1,6):
     km = KMeans(n_clusters=i, max_iter=500)
     # Perform K-means clustering on data X
     km.fit(x)
@@ -67,11 +64,11 @@ for i in range(1,7):
     cost.append(km.inertia_)
 
 # Plot the cost against K values
-plt.plot(range(1, 7), cost, color='b', linewidth='4')
+plt.plot(range(1, 6), cost, color='b', linewidth='4')
 plt.xlabel('Value of K')
 plt.ylabel('Squared Error (Cost)')
 plt.title('Elbow Method')
-plt.savefig('elbow-method.png')
+plt.savefig('plots/unsupervised/elbow-method.png')
 plt.figure()
 
 # Elbow method explanation: the bend in the elbow tells you how many
@@ -81,49 +78,54 @@ plt.figure()
 ###### Step 2: Cluster analysis
 
 # K-Means clustering
-km = KMeans(n_clusters=3, random_state=111)
+total_clusters = 4
+km = KMeans(n_clusters=total_clusters, random_state=111)
 
 # Perform K-means clustering on data X
 km.fit(x)
 
 # Display centroids
-centroids = pd.DataFrame(km.cluster_centers_, columns=['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width'])
+centroids = pd.DataFrame(
+    km.cluster_centers_,
+    columns=['acc_x','acc_y','acc_z','gyr_x','gyr_y','gyr_z','mag_x','mag_y','mag_z','label']
+)
+
 print(centroids)
 
 # Predictions label colors
-color1 = np.array(['green', 'red', 'blue'])
-pred_y = pd.DataFrame(km.labels_, columns=['Target']) # Membership matrix
+color1 = np.array(['gray', 'green', 'red', 'blue'])
+pred_y = pd.DataFrame(km.labels_, columns=['label']) # Membership matrix
 
-# Data visualization - before and after clustering for sepal length vs. sepal width
+# Data visualization - before and after clustering for acc_x vs. acc_y
 fig, axes = plt.subplots(1, 2, figsize=(12,4))
-axes[0].scatter(x['Sepal Length'], x['Sepal Width'], c=colors[y['Target']], s=50)
-axes[1].scatter(x['Sepal Length'], x['Sepal Width'], c=color1[pred_y['Target']], s=50)
-axes[1].scatter(centroids['Sepal Length'], centroids['Sepal Width'], c='k', s=70)
-axes[0].set_xlabel('Sepal length', fontsize=14)
-axes[0].set_ylabel('Sepal width', fontsize=14)
+axes[0].scatter(x['acc_x'], x['acc_y'], c=colors[x['label']], s=50)
+axes[1].scatter(x['acc_x'], x['acc_y'], c=color1[pred_y['label']], s=50)
+axes[1].scatter(centroids['acc_x'], centroids['acc_y'], c='k', s=70)
+axes[0].set_xlabel('acc_x', fontsize=14)
+axes[0].set_ylabel('acc_y', fontsize=14)
 axes[0].set_title('Before K-Means clustering', fontsize=14)
 
-axes[1].set_xlabel('Sepal length', fontsize=14)
-axes[1].set_ylabel('Sepal width', fontsize=14)
+axes[1].set_xlabel('acc_x', fontsize=14)
+axes[1].set_ylabel('acc_y', fontsize=14)
 axes[1].set_title('After K-Means clustering', fontsize=14)
 
-fig.savefig('iris-clustering-SLSW.png')
+fig.savefig(f'plots/unsupervised/phystherapy-clustering-AXAY-k{total_clusters}.png')
 plt.figure()
 
-# Data visualization - before and after clustering for petal length vs. petal width
+# Data visualization - before and after clustering for acc_x vs. mag_x
 fig, axes = plt.subplots(1, 2, figsize=(12,4))
-axes[0].scatter(x['Petal Length'], x['Petal Width'], c=colors[y['Target']], s=50)
-axes[1].scatter(x['Petal Length'], x['Petal Width'], c=color1[pred_y['Target']], s=50)
-axes[1].scatter(centroids['Petal Length'], centroids['Petal Width'], c='k', s=70)
-axes[0].set_xlabel('Petal length', fontsize=14)
-axes[0].set_ylabel('Petal width', fontsize=14)
+axes[0].scatter(x['acc_x'], x['mag_x'], c=colors[x['label']], s=50)
+axes[1].scatter(x['acc_x'], x['mag_x'], c=color1[pred_y['label']], s=50)
+axes[1].scatter(centroids['acc_x'], centroids['mag_x'], c='k', s=70)
+axes[0].set_xlabel('acc_x', fontsize=14)
+axes[0].set_ylabel('mag_x', fontsize=14)
 axes[0].set_title('Before K-Means clustering', fontsize=14)
 
-axes[1].set_xlabel('Petal length', fontsize=14)
-axes[1].set_ylabel('Petal width', fontsize=14)
+axes[1].set_xlabel('acc_x', fontsize=14)
+axes[1].set_ylabel('mag_x', fontsize=14)
 axes[1].set_title('After K-Means clustering', fontsize=14)
 
-fig.savefig('iris-clustering-PLPW.png')
+fig.savefig(f'plots/unsupervised/phystherapy-clustering-AXMX-k{total_clusters}.png')
 
 # What's the lesson from all this? If the data is well separated, it is very
 # easy to get good results with k-means clustering classification.
@@ -152,7 +154,7 @@ for i in num_clusters:
     print(f'Davis Bouldin index for clutsers = {i} is {davies_bouldin_score(x, clabel)}')
 
     # Silhouette index gives the average value for all the samples
-    print(f'Silhouette index for clusters = {i} is {silhouette_score(x, clabel)}')
+    print(f'Silhouette index for clusters = {i} is {silhouette_score(x, clabel)}\n')
 
 # -------------------------------------------------------------------------|
 # Davis Bouldin index for clutsers = 2 is 0.40429283717304343 | <--- min   |
